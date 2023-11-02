ecd20c4373b25ed98bc5b95417d08bf8253088bd
Kerolos163 <kokoessa951@gmail.com>
2023-11-02 18:43:08 +0200

    Finish PayMent App

--- lib/Features/CheckOut/Presentation/View/Widget/PaymentBottomSheet.dart
+++ lib/Features/CheckOut/Presentation/View/Widget/PaymentBottomSheet.dart
@@ -1,5 +1,10 @@
 import 'dart:developer';
 
+import 'package:checkoutapp/Features/CheckOut/Data/Models/amount_model/amount_model.dart';
+import 'package:checkoutapp/Features/CheckOut/Data/Models/amount_model/details.dart';
+import 'package:checkoutapp/Features/CheckOut/Data/Models/item_list_model/item.dart';
+import 'package:checkoutapp/Features/CheckOut/Data/Models/item_list_model/item_list_model.dart';
+import 'package:checkoutapp/Features/CheckOut/Presentation/ViewModel/PaymentDetailsCubit/cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
@@ -50,65 +55,20 @@ class PaymentBottomSheet extends StatelessWidget {
                 txt: "Continue",
                 isLoading: state is StribeLoading ? true : false,
                 onTap: () {
-                  // PaymentIntentInputModel paymentIntentInputModel =
-                  //     PaymentIntentInputModel(
-                  //   amount: "10",
-                  //   currency: "USD",
-                  //   customerId:
-                  //       customerId, //come from Account Object Should Create when Create Account
-                  // );
-                  // StribeCubit.get(context).makePayment(
-                  //     paymentIntentInputModel: paymentIntentInputModel);
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => PaypalCheckoutView(
-                      sandboxMode: true,
-                      clientId: "YOUR CLIENT ID",
-                      secretKey: "YOUR SECRET KEY",
-                      transactions: const [
-                        {
-                          "amount": {
-                            "total": '100',
-                            "currency": "USD",
-                            "details": {
-                              "subtotal": '100',
-                              "shipping": '0',
-                              "shipping_discount": 0
-                            }
-                          },
-                          "description": "The payment transaction description.",
-                          "item_list": {
-                            "items": [
-                              {
-                                "name": "Apple",
-                                "quantity": 4,
-                                "price": '10',
-                                "currency": "USD"
-                              },
-                              {
-                                "name": "Pineapple",
-                                "quantity": 5,
-                                "price": '12',
-                                "currency": "USD"
-                              }
-                            ],
-                          }
-                        }
-                      ],
-                      note: "Contact us for any questions on your order.",
-                      onSuccess: (Map params) async {
-                        log("onSuccess: $params");
-                        Navigator.pop(context);
-                      },
-                      onError: (error) {
-                        log("onError: $error");
-                        Navigator.pop(context);
-                      },
-                      onCancel: () {
-                        print('cancelled:');
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ));
+                  if (PaymentDetailsCubit.get(context).selectedMethod == 0) {
+                    PaymentIntentInputModel paymentIntentInputModel =
+                        PaymentIntentInputModel(
+                      amount: "10",
+                      currency: "USD",
+                      customerId:
+                          customerId, //come from Account Object Should Create when Create Account
+                    );
+                    StribeCubit.get(context).makePayment(
+                        paymentIntentInputModel: paymentIntentInputModel);
+                  } else {
+                    var transactionData = getTransactionData();
+                    executedPaypal(context, transactionData);
+                  }
                 },
               );
             },
@@ -116,5 +76,55 @@ class PaymentBottomSheet extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void executedPaypal(
+      BuildContext context,
+      ({
+        AmountModel amountModel,
+        ItemListModel itemListModel
+      }) transactionData) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (BuildContext context) => PaypalCheckoutView(
+        sandboxMode: true,
+        clientId: ApiKeys.clientID,
+        secretKey: ApiKeys.paypalSecretKey,
+        transactions: [
+          {
+            "amount": transactionData.amountModel.toJson(),
+            "description": "The payment transaction description.",
+            "item_list": transactionData.itemListModel.toJson()
+          }
+        ],
+        note: "Contact us for any questions on your order.",
+        onSuccess: (Map params) async {
+          log("onSuccess: $params");
+          Navigator.pop(context);
+        },
+        onError: (error) {
+          log("onError: $error");
+          Navigator.pop(context);
+        },
+        onCancel: () {
+          print('cancelled:');
+          Navigator.pop(context);
+        },
+      ),
+    ));
+  }
+
+  ({AmountModel amountModel, ItemListModel itemListModel})
+      getTransactionData() {
+    AmountModel amountModel = AmountModel(
+      currency: "USD",
+      total: "40",
+      details: Details(subtotal: "40", shipping: "0", shippingDiscount: 0),
+    );
+    List<Item>? orders = [
+      Item(currency: "USD", name: "Apple", price: "10", quantity: 4)
+    ];
+
+    ItemListModel itemListModel = ItemListModel(items: orders);
+    return (amountModel: amountModel, itemListModel: itemListModel);
   }
 }
